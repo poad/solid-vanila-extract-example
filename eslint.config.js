fe4eb23fadaddd6961344f26732c9b34e59480a1
Kenji Saito <ken-yo@mbr.nifty.com>
2024-09-09 21:39:32 +0900

    Bumps node modules

--- eslint.config.js
+++ eslint.config.js
@@ -1,40 +1,64 @@
-import eslintConfigPrettier from 'eslint-config-prettier';
-import eslintImport from "eslint-plugin-import";
-import typeScriptESLintParser from '@typescript-eslint/parser';
-import solid from 'eslint-plugin-solid';
-import typeScriptESLint from '@typescript-eslint/eslint-plugin';
+// @ts-check
 
-export default [
+import eslint from '@eslint/js';
+import stylistic from '@stylistic/eslint-plugin';
+import stylisticTs from '@stylistic/eslint-plugin-ts';
+import stylisticJsx from '@stylistic/eslint-plugin-jsx';
+import tseslint from 'typescript-eslint';
+// @ts-expect-error ignore type errors
+import importPlugin from 'eslint-plugin-import';
+import pluginPromise from 'eslint-plugin-promise';
+import solid from 'eslint-plugin-solid';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
+  pluginPromise.configs['flat/recommended'],
   {
-    plugins: {
-      eslintImport,
-    },
-    rules: {
-      "eslintImport/default": "error",
-    },
+    ignores: [
+      '**/*.d.ts',
+      '*.{js,jsx}',
+      'src/tsconfig.json',
+      'src/stories',
+      '**/*.css',
+      'node_modules/**/*',
+      'dist',
+    ],
   },
   {
-    rules: {
-      semi: ["error", "always"],
+    files: ['src/**/*.{ts,tsx}'],
+    ...importPlugin.flatConfigs.recommended,
+    ...importPlugin.flatConfigs.typescript,
+    languageOptions: {
+      parser: tseslint.parser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
     },
-  },
-  {
     plugins: {
-      typeScriptESLint,
+      '@stylistic': stylistic,
+      '@stylistic/ts': stylisticTs,
+      '@stylistic/jsx': stylisticJsx,
       solid,
     },
-    files: ["./src/*.js", "./src/*.ts", "./src/*.jsx", "./src/*.tsx"],
-    languageOptions: {
-      globals: {
-        Atomics: 'readonly',
-        SharedArrayBuffer: 'readonly'
+    settings: {
+      'import/parsers': {
+        espree: ['.js', '.cjs', '.mjs'],
+        '@typescript-eslint/parser': ['.ts'],
       },
-      parser: typeScriptESLintParser,
+      'import/internal-regex': '^~/',
+      'import/resolver': {
+        node: true,
+        typescript: true,
+      },
     },
-    linterOptions: {
-      noInlineConfig: true,
-      reportUnusedDisableDirectives: true,
+    rules: {
+      '@stylistic/semi': 'error',
+      '@stylistic/ts/indent': ['error', 2],
+      '@stylistic/jsx/jsx-indent': ['error', 2],
+      'comma-dangle': ['error', 'always-multiline'],
+      'quotes': ['error', 'single'],
+      semi: ['error', 'always'],
     },
   },
-  eslintConfigPrettier,
-];
+);
